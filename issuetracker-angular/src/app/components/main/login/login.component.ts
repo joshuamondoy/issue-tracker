edbde12c806664f62ffacac163d80dafe56fca6c
Joshua Mondoy <joshuamondoy24@gmail.com>
2022-11-09 20:31:18 +0800

    add landing page

--- issuetracker-angular/src/app/components/main/login/login.component.ts
+++ issuetracker-angular/src/app/components/main/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faCaretLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  faArrowLeft = faArrowLeft;
+  faCaretLeft = faCaretLeft;
+  isRegistered: boolean = true;
+  getStarted: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(formValues: NgForm) {}
+
+  toggleRegister() {
+    this.isRegistered = !this.isRegistered;
   }
 
+  toggleForm() {
+    this.getStarted = !this.getStarted;
+  }
 }
